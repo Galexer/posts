33d76c2a2eda9e440e2c53afa4cecfbe5e0f34c1
Galina Ry <galya.rybynok@gmail.com>
2022-08-15 18:29:46 +0300

    tests!

---
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -0,0 +1,54 @@
+package ru.netology
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import ru.netology.WallService.add
+import ru.netology.WallService.update
+
+class WallServiceTest {
+
+    @Test
+    fun addPost() {
+        val likes1 = Like(0,false,true)
+        val post1 = Post(0,0, 0,11-8-2022, likes = likes1)
+
+        val result = add(post1)
+
+        assertEquals(1, result.id)
+    }
+
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
+
+    @Test
+    fun updateGood() {
+        val likes1 = Like(userLikes = false, canLike = true)
+        add(Post(0,0, 0,11_08_2022, likes =  likes1))
+        val likes2 = Like(userLikes = false, canLike = true)
+        add(Post(0,1, 1,11_8_2022, likes = likes2))
+        val likes3 = Like(0,false,true)
+        add(Post(0,2, 2,11_8_2022, likes =  likes3))
+
+        val update = Post(3,2, 2,11-8-2022, "good post", likes =  likes3)
+
+        val result = update(update)
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun updateFalse() {
+        val likes1 = Like(userLikes = false, canLike = true)
+        add(Post(0,0, 0,11-8-2022, likes =  likes1))
+        val likes2 = Like(userLikes = false, canLike = true)
+        add(Post(0,1, 1,11-8-2022, likes = likes2))
+        val likes3 = Like(userLikes = false, canLike = true)
+
+        val update = Post(3,2, 2,11-8-2022, "another good post", likes = likes3)
+
+        val result = update(update)
+        assertEquals(false, result)
+    }
+}
